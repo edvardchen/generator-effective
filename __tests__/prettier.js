bf590d24bc32a4d8b8d2b06aa69867e7b4c6d5a3
edvardchen <>
2019-05-15 22:14:43 +0800

    feat: make subgenerator prettier integrate with eslint

--- __tests__/prettier.js
+++ __tests__/prettier.js
@@ -2,16 +2,42 @@
 const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
+const fs = require('fs');
 
 describe('generator-effective:prettier', () => {
-  beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/prettier'));
+  describe('blank', () => {
+    beforeAll(() => {
+      return helpers.run(path.join(__dirname, '../generators/prettier'));
+    });
+
+    it('creates files', () => {
+      assert.fileContent(
+        'package.json',
+        /"*\.\{ts,css,md,js\}": \[\s+"prettier --write",\s+"git add"\s+\]/m
+      );
+    });
   });
 
-  it('creates files', () => {
-    assert.fileContent(
-      'package.json',
-      /"*\.\{ts,css,md,js\}": \[\s+"prettier --write",\s+"git add"\s+\]/m
-    );
+  describe('integrate with eslint', () => {
+    function createEslintrc() {
+      const done = this.async();
+      fs.writeFile(path.resolve('./.eslintrc'), '', done);
+    }
+
+    beforeAll(() => {
+      return helpers
+        .run(path.join(__dirname, '../generators/prettier'))
+        .withPrompts({
+          formatWay: 1
+        })
+        .inTmpDir(createEslintrc);
+    });
+
+    it('creates files', () => {
+      assert.fileContent(
+        'package.json',
+        /"*\.\{ts,css,md,js\}": \[\s+"eslint --fix",\s+"git add"\s+\]/m
+      );
+    });
   });
 });
--- generators/prettier/index.js
+++ generators/prettier/index.js
@@ -2,10 +2,14 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const cosmiconfig = require('cosmiconfig');
+
+const formatCommands = ['prettier --write', 'eslint --fix'];
 
 module.exports = class extends Generator {
   initializing() {
     this.composeWith(require.resolve('../lint-staged'));
+    this.findEslintConfig();
   }
 
   prompting() {
@@ -17,24 +21,54 @@ module.exports = class extends Generator {
         )} generator!`
       )
     );
+
+    if (this.eslintConfig) {
+      const prompts = [
+        {
+          type: 'list',
+          name: 'formatWay',
+          message: 'Which way would you like to format code?',
+          choices: formatCommands,
+          default: 0
+        }
+      ];
+
+      return this.prompt(prompts).then(props => {
+        // To access props later use this.props.someAnswer;
+        this.props = props;
+      });
+    }
   }
 
   configuring() {
-    this.fs.copy(
-      this.templatePath('.prettierrc'),
-      this.destinationPath('.prettierrc')
-    );
+    const configPath = this.destinationPath('.prettierrc');
+    if (!this.fs.exists(configPath)) {
+      this.fs.copy(this.templatePath('.prettierrc'), configPath);
+    }
   }
 
   writing() {
+    const format = formatCommands[this.props ? this.props.formatWay : 0];
     this.fs.extendJSON(this.destinationPath('package.json'), {
+      scripts: {
+        format
+      },
       'lint-staged': {
-        '*.{ts,css,md,js}': ['prettier --write', 'git add']
+        '*.{ts,css,md,js}': [format, 'git add']
       }
     });
   }
 
   install() {
     this.npmInstall(['prettier'], { 'save-dev': true });
+  }
+
+  findEslintConfig() {
+    const root = this.destinationRoot();
+    const explorer = cosmiconfig('eslint', {
+      ignoreEmptySearchPlaces: false,
+      stopDir: root
+    });
+    this.eslintConfig = explorer.searchSync();
   }
 };
