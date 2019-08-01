3ccf3362b5b3c3ac00802fe7c4ef6a896e8da74a
edvardchen <pigeon73101@gmail.com>
2019-08-01 21:31:29 +0800

    fix: transfer outDir to absolute path

--- generators/grpc/index.js
+++ generators/grpc/index.js
@@ -95,8 +95,9 @@ module.exports = class extends Generator {
     if (this.props.codegen === STATIC) {
       const protoDir = this.destinationPath(this.props.protoDir);
       if (fs.existsSync(protoDir)) {
-        const { outDir } = this.props;
+        let { outDir } = this.props;
         mkdirp(outDir);
+        outDir = this.destinationPath(outDir);
         // grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./static_codegen/ --grpc_out=./static_codegen --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` protos/helloworld.proto
         const binPath = path.join(
           __dirname,
